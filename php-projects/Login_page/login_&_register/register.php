f986a2109b54844c52a63f54457f27a3099cfba9
Judasama69 <kenjiepen14@gmail.com>
2025-12-29 17:19:04 +0800

    commit pota ai

---
+++ php-projects/Login_page/login_&_register/register.php
@@ -0,0 +1,27 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "user_login_system");
+
+if ($conn->connect_error) die("Connection failed");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    $check = $conn->prepare("SELECT username FROM users WHERE username = ?");
+    $check->bind_param("s", $user);
+    $check->execute();
+
+    if ($check->get_result()->num_rows > 0) {
+        header("Location: index.html?error=taken");
+        exit();
+    } else {
+        $hash = password_hash($pass, PASSWORD_DEFAULT);
+        $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+        $stmt->bind_param("ss", $user, $hash);
+
+        if ($stmt->execute()) {
+            header("Location: index.html?status=success");
+            exit();
+        }
+    }
+}
--- php-projects/scratch/index.php
+++ php-projects/scratch/index.php
@@ -1,13 +1,10 @@
 <?php
 
-$favcolor = $_GET['color'] ?? 'red';
 
-echo match ($favcolor) {
-    "red" => "Your favorite color is red!",
-    "blue" => "Your favorite color is blue!",
-    "green" => "Your favorite color is green!",
-    default => "Your favorite color is neither red, blue, nor green!",
-};
 
-//this is a comment
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . "<br>";
+}
+
+
 
