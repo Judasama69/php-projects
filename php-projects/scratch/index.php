a0c5508d1833584d0a278bd0cc3d85f68a68d915
Judasama69 <kenjiepen14@gmail.com>
2025-12-31 16:47:51 +0800

    commit

--- php-projects/scratch/index.php
+++ php-projects/scratch/index.php
@@ -1,10 +1,28 @@
 <?php
 
+include 'Foods.php';
+
+$Burger = new Foods();
+$Hotdog = new Foods();
+
+$Burger->set_name('Burger');
+$Burger->set_type('Junk Food');
+$Burger->set_quantity(2);
 
 
-for ($i = 1; $i <= 10; $i++) {
-    echo $i . "<br>";
-}
+$Hotdog->set_name('Hotdog');
+$Hotdog->set_type('Junk Food');
+$Hotdog->set_quantity(3);
 
-
+echo $Burger->get_name();
+echo "<br>";
+echo $Burger->get_type();
+echo "<br>";
+echo $Burger->get_quantity();
+echo "<br>";
+echo $Hotdog->get_name();
+echo "<br>";
+echo $Hotdog->get_type();
+echo "<br>";
+echo $Hotdog->get_quantity();
 
